73b415667d0834a6149e1e80e4f333ac3d8bee6e
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-09-11 12:45:15 +0700

    add

---
+++ src/findMinUseMethod.java
@@ -0,0 +1,17 @@
+public class findMinUseMethod {
+    public static void main(String[] args) {
+        int[] arr = {3,4,6,8,9,1,7};
+        int index = minArr(arr);
+        System.out.println("the smallest element in the list is: "+ arr[index]);
+    }
+    public  static  int minArr(int[] arr){
+        int min = arr[0];
+        int index= 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min){
+                min = arr[i];
+                index = i;
+            }
+        } return index;
+    }
+}
